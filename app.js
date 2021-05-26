0430f7c03f8e91e772cdac6d58cc0e39d6982e67
yunisu.thn <yunis.thn@gmail.com>
2021-05-26 08:52:07 +0200

    update

--- app.js
+++ app.js
@@ -6,12 +6,13 @@ var alert = require('alert')
 var http = require('http')
 
 http.createServer(function (req, res) {
-    
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    var deskpath = path.join(home, 'Desktop', 'downloaded')
-    alert('directory create')
-    fs.mkdirSync(deskpath)
-    res.write('Hello new app')
-    res.end();
+    if (req.url == '/upload') {
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        var deskpath = path.join(home, 'Desktop', 'downloaded')
+        alert('directory create')
+        fs.mkdirSync(deskpath)
+        res.write('Hello new app')
+        res.end();
+    }
 
   }).listen(8080);
