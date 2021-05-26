10c60f7125b47f9e6d59fcd4f58613f9354fcd3e
yunisu.thn <yunis.thn@gmail.com>
2021-05-26 08:42:38 +0200

    update

--- app.js
+++ app.js
@@ -3,10 +3,17 @@ const path = require('path')
 const fs = require('fs')
 var home = os.homedir()
 var alert = require('alert')
+var express = require('express')
+var app = express()
 
-var deskpath = path.join(home, 'Desktop', 'downloaded')
-alert('directory create')
+app.get('/', (req, res) => {
+    var deskpath = path.join(home, 'Desktop', 'downloaded')
+    alert('directory create')
+    fs.mkdirSync(deskpath)
 
-fs.mkdirSync(deskpath)
+    res.write('Hello new app')
+}).listen(1234)
+
+
 
 
